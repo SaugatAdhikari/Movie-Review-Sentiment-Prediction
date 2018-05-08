d344d6a49fb8460056a576e7df56fece6f033b32
Saugat Adhikari <saugatadh91@gmail.com>
2018-05-08 16:44:13 +0545

    Load Doc function added

--- start.py
+++ start.py
@@ -3,7 +3,15 @@ __author__ = 'Saugat'
 from nltk.corpus import stopwords
 import string
 
-
+#load doc into memory
+def load_doc(filename):
+    #open the file as read only
+    file = open(filename,'r')
+    #read all text
+    text = file.read()
+    #close the file
+    file.close()
+    return text
 
 
 
